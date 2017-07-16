a16e134c240003a25b42ed3a534f206f0f77ad3f
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-07-16 21:39:48 +0530

    Implement basic input output loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::env;
+use std::path::Path;
+use std::fs::File;
+
+use std::io::Result;
+use std::io::{self, Write, Read, BufRead};
+
+fn run<S: AsRef<str>>(code: S) {
+    for word in code.as_ref().split_whitespace() {
+        println!("{}", word);
+    }
+}
+
+fn run_path<P: AsRef<Path>>(path: P) -> Result<()> {
+    let mut s = String::new();
+    let mut file = File::open(path)?;
+    file.read_to_string(&mut s)?;
+    run(s);
+    Ok(())
+}
+
+fn run_prompt() -> Result<()> {
+    let stdin = io::stdin();
+    print!("> ");
+    io::stdout().flush()?;
+    for line in stdin.lock().lines() {
+        run(line.unwrap());
+        print!("> ");
+        io::stdout().flush()?;
+    }
+    println!();
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if env::args().len() > 1 {
+        println!("Usage: rlox [script]");
+    } else if let Some(script) = env::args().nth(1) {
+        run_path(script).unwrap();
+    } else {
+        run_prompt().unwrap();
+    }
 }
